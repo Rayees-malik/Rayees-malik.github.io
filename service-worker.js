c2abf372ca4888a53d218af05835c0e98d4adaba
Rayees Malik <rayeesmalik19@gmail.com>
2020-05-05 01:56:14 -0400

    Changed Browser Router to Hash Router

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efa816ec518eeeb48243a102c6721e27.js"
+  "/precache-manifest.e44238f5eaec75468017378a6205405e.js"
 );
 
 self.addEventListener('message', (event) => {
